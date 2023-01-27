20f5ff306903cc3a37c38beb7b79296b64ee3a4d
Jascent Pearl G. Navarro <111488181+jassynavarro@users.noreply.github.com>
2023-01-27 10:57:37 +0800

    Create Arrays.java

---
+++ Codes/Arrays.java
@@ -0,0 +1,19 @@
+package Codes;
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		String email[] = {"scentpearl@gmail.com", "jascentpearlnavarro@gmail.com"};
+		String userName[] = {"scentPearl", "jascentPearl"};
+		int passWord[] = {12345, 67890};
+		
+		int numOfIndex = 0;
+		
+		System.out.println("Index: " 	+ numOfIndex);
+		System.out.println("Email: " 	+ email[numOfIndex]);
+		System.out.println("username: " + userName[numOfIndex]);
+		System.out.println("password: " + passWord[numOfIndex]);
+
+	}
+
+}
